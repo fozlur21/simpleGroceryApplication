a898a326b53ad138cf879a1c1d8838f171c345c1
Fozlur <fozlur4022@gmail.com>
2023-10-06 11:45:56 +0600

    final version

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# React + Vite
+Description:
+    The Simple Grocery Application is a user-friendly digital tool designed to streamline your grocery shopping experience. Developed using the popular ReactJs framework, this application offers a modern and intuitive interface that makes creating and managing your shopping list.
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Key Features:
 
-Currently, two official plugins are available:
+User-Friendly Interface:
+    The application boasts an easy-to-navigate interface that ensures users of all tech levels can quickly adapt to and utilize its features.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Intuitive Shopping List:
+    With just a few clicks, you can add, edit, or remove items from your shopping list. The application simplifies the process of organizing your groceries, ensuring that you don't forget any essentials.
+
+Real-time Updates:
+    The application allows for real-time updates, ensuring that any changes you make to your shopping list are instantly reflected across all your devices, so you can access your list from anywhere.
+
+Checklist and Quantity Tracking:
+    Keep track of quantities for each item and mark them off as you shop, ensuring you never miss an item while at the store.
--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import "./App.css";
 import cartImg from "./assets/grocery-cart.png";
 
 const App = () => {
-  // State for the input value and the list of grocery items
   const [inputValue, setInputValue] = useState("");
   const [groceryItems, setGroceryItems] = useState([]);
   const [isCompleted, setIsCompleted] = useState(false);
 
-  // Function to handle changes in the input field
   const handleChangeValue = (e) => {
     setInputValue(e.target.value);
   };
 
-  //! status update
   useEffect(() => {
     showedCompleteStatus();
-  }, [groceryItems])
-  //! Function to add grocery items when the Enter key is pressed
+  }, [groceryItems]);
+
+
   const addGroceryItems = (e) => {
     if (e.key === "Enter") {
       if (inputValue) {
@@ -25,43 +23,36 @@ const App = () => {
         const itemIndex = updatedGroceryList.findIndex(item => item.name === inputValue);
 
         if (itemIndex === -1) {
-          //! Add a new item to the list
+
           updatedGroceryList.push({
             name: inputValue,
             quantity: 1,
             completed: false
           });
         } else {
-          //! Increment the quantity if the item already exists
+
           updatedGroceryList[itemIndex].quantity++;
         }
-
-        //! Update the state with the new grocery items list and clear the input
         setGroceryItems(updatedGroceryList);
         setInputValue("");
       }
     }
   };
 
-  //! Function to remove an item from the grocery list
   const removeItem = (name) => {
-    //! Create a new list without the item to be removed
     const updatedGroceryList = [...groceryItems].filter(
       (item) => item.name !== name
     );
-
-    //! Update the state with the new grocery items list
     setGroceryItems(updatedGroceryList);
   }
 
-  //! Function to render the grocery list
   const renderGroceryList = () => {
     return groceryItems.map((item, inx) => (
       <li key={inx}>
         <div className='container'>
           <input type="checkbox"
             onChange={(e) => {
-              // console.log(e.target.checked);
+
               updateCompleteStatus(e.target.checked, inx)
             }}
             value={item.completed}
@@ -112,6 +103,7 @@ const App = () => {
               type="text"
               placeholder='Add an Item'
               className='item-input'
+
               onChange={handleChangeValue}
               onKeyDown={addGroceryItems}
               value={inputValue}
